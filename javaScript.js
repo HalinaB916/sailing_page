2a6b9a76a99721cad81b156c0af4bf419d118eff
HalinaB916 <g.borsak@gmail.com>
2024-03-14 11:58:19 +0100

    Added JavaScript

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+function buy() {
+      let name = prompt("What is your name?");
+      let email = prompt("What is your email address?");
+      let emoji = prompt("What is your favorite emoji?");
+
+      alert("Thank you " + "! We'll be in touch by email, meanwhile have lot of Yogurt! " + emoji);
+}
+let buyButton = document.querySelector(".buy-button");
+buyButton.addEventListener("click", buy); 
