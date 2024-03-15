ab56b98da8e2bcc8bbcb570ed7707189a85af821
HalinaB916 <g.borsak@gmail.com>
2024-03-15 10:14:54 +0100

    Added name to javaScript

--- javaScript.js
+++ javaScript.js
@@ -3,7 +3,7 @@ function buy() {
       let email = prompt("What is your email address?");
       let emoji = prompt("What is your favorite emoji?");
 
-      alert("Thank you " + "! We'll be in touch by email, meanwhile have lot of Yogurt! " + emoji);
+      alert("Thank you, " + name + "! We'll be in touch by email, meanwhile have lot of Yogurt! " + emoji);
 }
 let buyButton = document.querySelector(".buy-button");
 buyButton.addEventListener("click", buy); 
